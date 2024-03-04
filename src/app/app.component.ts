83fec299de303c9aef7a4060ef0643c0c63cb529
ZlabiDev <zlabi.dev@gmail.com>
2024-03-04 20:46:39 +0100

    added content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'zd-daily-work-timer';
+    protected calculatedHours: string = '0';
+    protected startTime: string;
+    protected endTime: string;
+    protected freeTime: string = '30';
+
+    onCalculate() {
+        let startTimeDate = new Date();
+        startTimeDate.setHours(+this.startTime.split(':')[0]);
+        startTimeDate.setMinutes(+this.startTime.split(':')[1]);
+
+        let endTimeDate = new Date();
+        endTimeDate.setHours(+this.endTime.split(':')[0]);
+        endTimeDate.setMinutes(+this.endTime.split(':')[1]);
+
+        // Calculate the time difference in milliseconds
+        let hoursTmp = endTimeDate.getTime() - startTimeDate.getTime();
+
+        // Convert milliseconds to hours
+        hoursTmp = hoursTmp / (1000 * 60 * 60);
+
+        // Subtract free time
+        hoursTmp -= +this.freeTime / 60; // Convert freeTime from minutes to hours
+
+        // If calculated hours are negative, set them to 0
+        if (hoursTmp < 0) {
+            hoursTmp = 0;
+        }
+
+        // Assign the calculated hours to the variable
+        this.calculatedHours = hoursTmp.toFixed(2);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatNativeDateModule} from '@angular/material/core';
+import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {NgxMaterialTimepickerModule} from "ngx-material-timepicker";
+import {MatButtonModule} from "@angular/material/button";
+
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatNativeDateModule,
+        FormsModule,
+        MatDatepickerModule,
+        NgxMaterialTimepickerModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
